48f63da3282279f7b17900629c5586209576d9d0
Eric DJAVID <edjavid@e2r2p19.clusters.42paris.fr>
2021-11-18 11:34:46 +0100

    init

--- prompt_msg.c
+++ prompt_msg.c
@@ -16,7 +16,7 @@ char	*prompt_msg(void)
 {
 	char	*cmd_line;
 
-	rl_on_new_line();
+	// rl_on_new_line();
 	cmd_line = readline("\033[0;32m->  \033[0;34mMinishell \033[0;m");
 	add_history(cmd_line);
 	return (cmd_line);
